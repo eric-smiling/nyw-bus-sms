5a9fe1c58c5646b50839b0a515f87d5b562ac954
Eric Smiling <esmiling@Erics-MBP.home>
2015-11-21 14:20:45 -0500

    upgrades

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 // TODO: prevent spam
 // TODO: keep list of active SMS users
+// TODO: hide/don't count incorrect bus ids
+// TODO: keep track of actual bus id based on entrance entry
 
 var express = require('express');
 var router = express.Router();
@@ -13,20 +15,27 @@ router.get('/map', function(req, res) {
   res.render('../views/map.html');
 });
 
-router.post('/vehicles/:range_key', function(req, res, next) {
-  if(!nyw.isValidKey(req.params.range_key)) {
-    return res.send({"error": "invalid range key"});
+router.get('/poi-form', function(req, res) {
+  res.render('../views/poi-form.html', {
+    pois: nyw.getRanges()
+  });
+});
+
+router.post('/poi/vehicles', function(req, res, next) {
+  if(!nyw.isPOI(req.body.poi)) {
+    return res.send({"error": "invalid POI"});
   }
   if(!/^\d{10}$/.test(req.body.to)) {
     return res.send({"error": "invalid phone number"});
   }
   
   // validate
-  nyw.findVehiclesInRange(req.params.range_key, function(vehicles, positions){
+  nyw.findVehiclesInRange(req.body.poi, function(vehicles, positions){
     if(vehicles.length) {
+    /*
       sms.send({
         to: '+1' + req.body.to,
-        body: 'Bus arrived @ ' + req.params.range_key,
+        body: 'Bus arrived @ ' + req.body.poi,
         complete: function(error, message) {
           res.send({
             'vehicles': vehicles,
@@ -34,12 +43,21 @@ router.post('/vehicles/:range_key', function(req, res, next) {
           });
         }
       });
+    */
+      res.send({
+        'vehicles': vehicles,
+        'sms_sent': true
+      });
     } else {
       res.send({
         'vehicles': []
       });
     }
   });
+});
+
+router.get('/ranges', function(req,res) {
+  res.json(nyw.getRanges());
 });
 
 module.exports = router;
