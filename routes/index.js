c77f659155c321a9d590c147a1af7095c673d54f
Eric Smiling <esmiling@Erics-MacBook-Pro.local>
2015-11-21 19:31:21 -0500

    stuff

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 var rest = require('restler');
+var proxy = require('express-http-proxy');
+var zlib = require('zlib');
 var nyw = require('../lib/nyw');
 var sms = require('../lib/sms');
 var pois = nyw.getRanges();
+var JFID;
 
+
+//////////
+// SMS POI
+//////////
 router.get('/poi-form', function(req, res) {
   res.render('../views/poi-form.html', {
     pois: pois
@@ -36,6 +43,12 @@ router.post('/poi/vehicles', function(req, res, next) {
           });
         }
       });
+      // JF bus is the only bus that enters the back entrance
+      if(poi.key === 'back_entrance') {
+        // keep the ID in memory across requests
+        JFID = vehicles[0].o;
+        console.log('SMS::SET JF BUS ID to ' + JFID);
+      }
     } else {
       res.send({
         'vehicles': [],
@@ -46,6 +59,59 @@ router.post('/poi/vehicles', function(req, res, next) {
   });
 });
 
+
+///////////
+// FAUX MAP
+///////////
+router.get('/faux-map', function(req, res) {
+  res.render('../views/faux-map.html');
+});
+
+router.get('/faux-vehicle-positions', proxy('tds1.saucontech.com', {
+  forwardPath: function(req, res) {
+    return '/tds-map/nyw/nywvehiclePositions.do?id=52';
+  },
+  decorateRequest: function(req) {
+    req.headers['accept-encoding'] = 'identity';
+  },
+  intercept: function(rsp, data, req, res, callback) {
+    
+    var jfVehicle;
+    var filteredData;
+    
+    // parse saucontech response
+    data = JSON.parse(data.toString('utf8'));
+    
+    // a vehicle in the back entrance is the JF vehicle
+    jfVehicle = _.find(data, function(vehicle) {
+      return nyw.isInRange('back_entrance', vehicle);
+    });
+    
+    // if there is one
+    if(jfVehicle) {
+      // store its vehicle ID in memory for use by subsequent requests
+      JFID = jfVehicle.o;
+      console.log('FAUX::SET JFID to ' + JFID);
+    }
+    
+    // JFID may have been set by other requests
+    if(JFID) {
+      // if present, hide all other buses
+      filteredData = _.filter(data, function(item) {
+        return item.o == JFID;
+      });
+      if(filteredData.length) {
+        data = filteredData;
+      }
+    }
+    callback(null, JSON.stringify(data));
+  }
+}));
+
+
+////////
+// DEBUG
+////////
 router.get('/ranges', function(req,res) {
   res.json(nyw.getRanges());
 });
